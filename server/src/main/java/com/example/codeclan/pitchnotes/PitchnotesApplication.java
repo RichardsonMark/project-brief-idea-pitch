7b012b14a1770b986b94a98999b65385b8472ad0
Mark Richardson <mark.richardson4@gmail.com>
2021-02-16 22:00:41 +0000

    project folder structure set up with client and server. New React project added to client folder. New springinitializr Spring Boot project added to server folder

---
+++ server/src/main/java/com/example/codeclan/pitchnotes/PitchnotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.pitchnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitchnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitchnotesApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/example/codeclan/pitchnotes/PitchnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.pitchnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitchnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
