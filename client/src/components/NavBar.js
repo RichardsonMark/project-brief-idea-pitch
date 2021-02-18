119937d849096a6ddd3fe22d309d8844c4dc31e0
Mark Richardson <mark.richardson4@gmail.com>
2021-02-18 13:08:30 +0000

    react router added to front end, nav bar text set up and displays on localhost

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,29 @@
+import { Link, useHistory } from "react-router-dom";
+
+
+const NavBar = () => {
+    const history = useHistory();
+
+    const goBack = () => {
+        history.goBack();
+    }
+
+    return (
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+            </li>
+            <li>
+                <Link to="/authors">Authors</Link>
+            </li>
+            <li>
+                <Link to="/articles">Articles</Link>
+            </li>
+            <li>
+                <button onClick={goBack}>Back</button>
+            </li>
+        </ul>
+    );
+}
+
+export default NavBar;
